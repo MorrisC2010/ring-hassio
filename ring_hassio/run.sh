9dbb732f243e2faa791bb3ab9f6907134817edf0
BlakSpryte <cmorris565@gmail.com>
2023-09-18 23:28:12 -0500

    modified:   ring_hassio/Dockerfile modified:   ring_hassio/package-lock.json modified:   ring_hassio/package.json modified:   ring_hassio/run.sh

--- ring_hassio/run.sh
+++ ring_hassio/run.sh
@@ -4,5 +4,10 @@ export RING_REFRESH_TOKEN="$(jq --raw-output '.ring_refresh_token' $CONFIG_PATH)
 export CAMERA_NAME="$(jq --raw-output '.camera_name' $CONFIG_PATH)"
 export RING_PORT="$(jq --raw-output '.port' $CONFIG_PATH)"
 
-#cd /ring-hassio/ring_hassio
+echo $CONFIG_PATH
+echo $RING_REFRESH_TOKEN
+echo $RING_PORT
+echo $CAMERA_NAME
+
+cd ./ring_hassio
 node livestream.js
